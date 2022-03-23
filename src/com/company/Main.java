d1d4fdc90d52da4ff4aad239c96d6b777daebb78
Forcya <74520011+Forcya@users.noreply.github.com>
2022-03-23 13:53:13 +0300

    Новый принт3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Задачи 2-ого модуля");
-        System.out.println("Задачи 2-ого модуля");
-        System.out.println("Задачи 2-ого модуля");
-        System.out.println("commit init 3");
+        System.out.println("-------------------");
         //3.3.12
 //	    Human human1 = new Human();
 //        Human human2 = new Human();
